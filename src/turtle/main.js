170655535688ffe41c74573555be2eed1504b196
Brandon Bloom <brandon@brandonbloom.name>
2013-09-24 15:21:19 -0400

    Add a helpHtml option to main that goes in to a help area

--- src/turtle/main.js
+++ src/turtle/main.js
@@ -3,13 +3,23 @@ window.Turtle = require('./turtle');
 require('./answers'); //XXX Mutates Turtle
 var blocks = require('./blocks');
 
-window.turtleMain = function() {
+window.turtleMain = function(options) {
 
-  document.write(turtlepage.start({}, null, {
+  if (!options) {
+    options = {};
+  }
+
+  var config = {
     page: BlocklyApps.PAGE,
     level: BlocklyApps.LEVEL,
     baseUrl: BlocklyApps.BASE_URL
-  }));
+  };
+
+  if (options.helpHtml) {
+    config.helpHtml = options.helpHtml;
+  }
+
+  document.write(turtlepage.start({}, null, config));
 
   blocks.install(Blockly);
 
