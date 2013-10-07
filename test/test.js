e3be49489287270ec052c07719d93792e5bd7d94
Brandon Bloom <brandon@brandonbloom.name>
2013-10-07 15:13:32 -0400

    Configure mocha test framework with chai assertions

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+var turtleLevels = require('../src/turtle/levels');
+
+describe('Turtle Levels', function() {
+  it('does stuff', function() {
+    assert(true, "woot!");
+  });
+});
