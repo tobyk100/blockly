22c27bcff2230d9a0d7e3ad32bfce08420621c78
Brandon Bloom <brandon@brandonbloom.name>
2013-10-09 17:06:26 -0400

    Enumate puzzles for testing their solutions

--- test/test.js
+++ test/test.js
@@ -1,9 +1,30 @@
+var path = require('path');
+var fs = require('fs');
 var assert = require('chai').assert;
 
-var turtleLevels = require('../src/turtle/levels');
+//TODO: Validate that solutions match their puzzle's toolbox.
 
-describe('Turtle Levels', function() {
-  it('does stuff', function() {
-    assert(true, "woot!");
+['maze', 'turtle'].forEach(function(app) {
+  describe(app + ' levels', function() {
+
+    var levels = require('../build/js/' + app + '/levels');
+    Object.keys(levels).forEach(function(levelId) {
+
+      var xmlPath = path.join(__dirname, 'solutions', app, levelId + '.xml');
+      var description = 'level ' + levelId + ' solution';
+
+      if (fs.existsSync(xmlPath)) {
+        it(description, function() {
+          var xml = fs.readFileSync(xmlPath, 'utf8');
+          //TODO: Run solution and validate success
+          assert(false);
+        });
+      } else {
+        // Treat missing solutions as pending, not failing.
+        it(description);
+      }
+
+    });
+
   });
 });
