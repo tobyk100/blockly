cbc0f08ded45cb30201eda81e5517252f76c601d
Nan <nan@code.org>
2013-10-14 11:24:32 -0700

    Removing random buildings from level 1, 2, 3 in HoC.

--- src/maze/maze.js
+++ src/maze/maze.js
@@ -222,6 +222,12 @@ var drawMap = function() {
         } else {
           tile = 'null' + Math.floor(1 + Math.random() * 4);
         }
+
+        // For the first 3 levels in maze, only show the null0 image.
+        console.log(level);
+        if (level.id == '2_1' || level.id == '2_2' || level.id == '2_3') {
+          tile = 'null0';
+        }
       }
       var left = TILE_SHAPES[tile][0];
       var top = TILE_SHAPES[tile][1];
